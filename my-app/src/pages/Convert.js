9b543dc4c4d8178baf5000f3009e3c3b99d26843
Artyom <keloss94@mail.ru>
2024-01-28 19:14:34 +0700

    convert is done

--- my-app/src/pages/Convert.js
+++ my-app/src/pages/Convert.js
@@ -7,36 +7,51 @@ const API_URL = 'https://open.er-api.com/v6/latest/'
 function Convert() {
 
     const [currency, setCurrency] = useState([])
-
-    const [currentRate, setCurrentRate] = useState('RUB')
+    const [input, setInput] = useState(0)
+    const [output, setOutput] = useState(0)
+    const [options, setOptions]  = useState([])
+    const [from, setFrom] = useState('RUB')
+    const [to, setTo] = useState('USD')
 
     useEffect(() => {
-        fetch(`${API_URL}${currentRate}`)
+        fetch(`${API_URL}${from}`)
         .then(response => response.json())
         .then(data => setCurrency(Object.entries(data.rates)))
-    }, [currentRate])
+    }, [from])
+
+    useEffect(() => {
+        setOptions(Object.keys(currency));
+        convert();
+    }, [currency])
+
+    function convert() {
+        var rate = 0
+        {currency.map((item) => {
+            if (item[0] === to) {rate = item[1]}
+        })}
+        setOutput(input * rate);
+    }
 
     return (
         <>
             <div className="Convert">
                 <div className="amount">
                     <p>Amount</p>
-                    <input>
-                    </input>
+                    <input onChange = {(e) => {setInput(e.target.value)}}/>
                 </div>
 
                 <div className="from">
                     <p>From</p>
-                    <select defaultChecked={currentRate} onChange={e => setCurrentRate(e.target.value)}>
+                    <select defaultChecked={from} onChange={e => setFrom(e.target.value)} value = {from}>
                         {currency.map(item => <option key = {item[0]} value={item[0]}>
-                            {item[0]}
+                            {item[0]} 
                         </option>)}
                     </select>
                 </div>
 
                 <div className="to">
                     <p>To</p>
-                    <select defaultChecked={currentRate} onChange={e => setCurrentRate(e.target.value)}>
+                    <select defaultChecked={from} onChange={e => setTo(e.target.value)} value = {to}>
                         {currency.map(item => <option key = {item[0]} value={item[0]}>
                             {item[0]}
                         </option>)}
@@ -44,7 +59,8 @@ function Convert() {
                 </div>
 
                 <div>
-                    <button className="btn">Convert</button>
+                    <button onClick={() => { convert() }}>Convert</button>
+                    <p className="result">{input + " " + from + "=" + output.toFixed(2) + " " + to}</p>
                 </div>
             </div>
         </>
